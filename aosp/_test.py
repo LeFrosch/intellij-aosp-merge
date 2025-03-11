64e7be6459d470fd6f5f76b80a594633bab6fef6
Daniel Brauner <daniel.brauner@jetbrains.com>
2025-03-11 11:11:04 +0100

    add buildonly flag for testing

--- aosp/_test.py
+++ aosp/_test.py
@@ -11,6 +11,13 @@ TEST_CASES = [
 ]
 
 
+BUILD_CASES = [
+    ('//clwb:clwb_bazel_zip', 'clion-oss-latest-stable'),
+    ('//ijwb:ijwb_bazel_zip', 'intellij-ue-oss-latest-stable'),
+    ('//querysync', 'intellij-ue-oss-latest-stable'),
+]
+
+
 def bazel_test(repo: str, target: str, ij_product: str):
     """
     Runs a bazel test command for the given target and ij_product.
@@ -48,12 +55,55 @@ def bazel_test(repo: str, target: str, ij_product: str):
             exit("test aborted")
 
 
+def bazel_build(repo: str, target: str, ij_product: str):
+    while True:
+        log('executing build %s' % target)
+
+        success = subprocess.run(
+            [
+                'bazel',
+                'build',
+                target,
+                '--define=ij_product=%s' % ij_product,
+                '--disk_cache=/tmp/bazel_cache',
+            ],
+            cwd=repo,
+            stderr=sys.stdout,
+            stdout=sys.stdout,
+        ).returncode == 0
+
+        if success:
+            log('build %s passed' % target)
+            break
+
+        result = choose(
+            title='build %s failed, press enter to retry' % target,
+            options=[
+                '[r] retry',
+                '[a] abort',
+            ],
+        )
+
+        if result == "a":
+            exit("test aborted")
+
+
 def configure(parser: argparse.ArgumentParser):
-    pass
+    parser.add_argument(
+        '--buildonly',
+        action='store_true',
+        help='run build the targets',
+        default=False,
+    )
 
 
 def execute(args: argparse.Namespace):
-    for (target, product) in TEST_CASES:
-        bazel_test(args.repo, target, product)
+    if (args.buildonly):
+        for (target, product) in BUILD_CASES:
+            bazel_build(args.repo, target, product)
+        log('all builds passed')
 
-    log('all tests passed')
+    else:
+        for (target, product) in TEST_CASES:
+            bazel_test(args.repo, target, product)
+        log('all tests passed')
